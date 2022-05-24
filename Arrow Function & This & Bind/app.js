448995091986780adbd2e9a5c32a159752606e49
zehrabarann <brnzehraa@gmail.com>
2022-05-24 14:05:46 +0300

    Arrow Function, this and bind

---
+++ Arrow Function & This & Bind/app.js
@@ -0,0 +1,20 @@
+const person = {
+    age:25,
+    // tellAge : function() {
+
+    //     //this - Person
+    //     console.log(this);
+    //     console.log(this.age);
+    // }.bind(this)
+
+    tellAge: () => {
+        console.log(this);
+        console.log(this.age);
+    }
+
+
+       //this - Window 
+
+}
+
+person.tellAge();
